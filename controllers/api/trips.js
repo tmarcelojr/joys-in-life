3af3962ea5e2649ca81bb345826fa4e90913f63e
Teo Marcelo <tmarcelojr@gmail.com>
2022-06-02 21:23:14 -0500

    Show, Update, and Delete routes functioning

--- controllers/api/trips.js
+++ controllers/api/trips.js
@@ -1,8 +1,8 @@
 const Trip = require('../../models/Trip')
 
+// Finds all trips
 async function index(req, res) {
     try {
-        // Finds all trips
         const trips = await Trip.find({})
         res.status(200).json(trips)
     } catch(e) {
@@ -10,9 +10,19 @@ async function index(req, res) {
     }
 }
 
+// Finds a trip
+async function show(req, res) {
+    try {
+        const foundTrip = await Trip.findById(req.params.id)
+        res.status(200).json(foundTrip)
+    } catch(e) {
+        res.status(400).json({msg: e.message})
+    }
+}
+
+// Adds new trip 
 async function create(req, res) {
     try {
-        // Adds new trip
         const createdTrip = await Trip.create(req.body)
         res.status(200).json(createdTrip)
     } catch(e) {
@@ -20,7 +30,30 @@ async function create(req, res) {
     }
 }
 
+// Update a trip
+async function update(req, res) {
+    try {
+        const updatedTrip = await Trip.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(updatedTrip)
+    } catch(e) {
+        res.status(400).json({msg: e.message})
+    }
+}
+
+// Deletes a trip
+async function remove(req, res) {
+    try {
+        const updatedTrip = await Trip.findByIdAndDelete(req.params.id)
+        res.status(200).json(updatedTrip)
+    } catch(e) {
+        res.status(400).json({msg: e.message})
+    }
+}
+
 module.exports = {
     index,
-    create
+    create,
+    show,
+    update,
+    remove
 }
--- routes/api/trips.js
+++ routes/api/trips.js
@@ -2,9 +2,15 @@ const express = require('express')
 const router = express.Router()
 const tripsCtrl = require('../../controllers/api/trips')
 
-// GET /api/trips
+// GET /api/v1/trips
 router.get('/', tripsCtrl.index)
-// POST /api/trips
+// GET /api/v1/trips/:id
+router.get('/:id', tripsCtrl.show)
+// POST /api/v1/trips
 router.post('/', tripsCtrl.create)
+// PUT /api/v1/trips/:id
+router.put('/:id', tripsCtrl.update)
+// DELETR /api/v1/trips/:id
+router.delete('/:id', tripsCtrl.remove)
 
 module.exports = router
